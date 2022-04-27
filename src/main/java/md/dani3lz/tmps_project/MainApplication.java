afc3348633bcda9225e7499bed0d88dd79d0b5c7
dani3lz <zavorot.daniel@isa.utm.md>
2022-04-27 18:15:12 +0200

    update

--- src/main/java/md/dani3lz/tmps_project/MainApplication.java
+++ src/main/java/md/dani3lz/tmps_project/MainApplication.java
@@ -12,6 +12,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
         stage.setTitle("PC Store");
         stage.setScene(scene);
         stage.show();
