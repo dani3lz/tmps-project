d7bfe71a488c5c451953d973b0a7db04d3013b68
dani3lz <zavorot.daniel@isa.utm.md>
2022-04-28 13:11:31 +0200

    update

--- src/main/java/md/dani3lz/tmps_project/StoreController.java
+++ src/main/java/md/dani3lz/tmps_project/StoreController.java
@@ -27,6 +27,23 @@ import java.util.ResourceBundle;
 
 public class StoreController implements Initializable {
     @FXML
+    private Button allBtn;
+    @FXML
+    private Button cpuBtn;
+    @FXML
+    private Button gpuBtn;
+    @FXML
+    private Button headphoneBtn;
+    @FXML
+    private Button keyboardBtn;
+    @FXML
+    private Button mouseBtn;
+    @FXML
+    private Button ramBtn;
+    @FXML
+    private Button speakerBtn;
+    // ------------------------------------------------------------
+    @FXML
     private ImageView componentImg;
     @FXML
     private Label componentName;
@@ -46,6 +63,7 @@ public class StoreController implements Initializable {
 
     private List<Component> components = new ArrayList<>();
     private MyListener myListener;
+    private Option option_selected = null;
 
 
     public void btnALL(){
@@ -81,10 +99,69 @@ public class StoreController implements Initializable {
     public void btnSearch() {
         String textInput = inputSearch.getText().toLowerCase();
         inputSearch.clear();
-        searchLabel.setVisible(true);
-        searchLabel.setText("Search: \"" + textInput + "\"");
-        Search.searchComponents(textInput);
-        initMethod(Option.SEARCH);
+        if(!textInput.equals("")) {
+            searchLabel.setVisible(true);
+            searchLabel.setText("Search: \"" + textInput + "\"");
+            Search.searchComponents(textInput);
+            initMethod(Option.SEARCH);
+        }
+    }
+
+    private void initMethod(Option option){
+        if(option != Option.SEARCH){
+            searchLabel.setVisible(false);
+        }
+        if(option_selected != option || option == Option.SEARCH) {
+            option_selected = option;
+            setButton(option);
+            grid.getChildren().clear();
+            List<Component> components;
+            components = InitComponents.getInstance().getData(option);
+
+            if (components.size() > 0) {
+                setCard(components.get(0));
+                myListener = new MyListener() {
+                    @Override
+                    public void onClickListener(Component component) {
+                        setCard(component);
+                    }
+                };
+            }
+
+            int column = 0;
+            int row = 1;
+
+            try {
+                for (Component component : components) {
+                    FXMLLoader fxmlLoader = new FXMLLoader();
+                    fxmlLoader.setLocation(getClass().getResource("component.fxml"));
+                    AnchorPane anchorPane = fxmlLoader.load();
+
+                    ComponentController componentController = fxmlLoader.getController();
+                    componentController.setData(component, myListener);
+
+                    if (column == 3) {
+                        column = 0;
+                        row++;
+                    }
+
+                    grid.add(anchorPane, column++, row);
+
+                    GridPane.setMargin(anchorPane, new Insets(10));
+                }
+
+                grid.setMinWidth(Region.USE_COMPUTED_SIZE);
+                grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                grid.setMaxWidth(Region.USE_PREF_SIZE);
+
+                grid.setMinHeight(Region.USE_COMPUTED_SIZE);
+                grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                grid.setMinHeight(Region.USE_PREF_SIZE);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void setCard(Component component){
@@ -93,61 +170,32 @@ public class StoreController implements Initializable {
         Image image = new Image(getClass().getResourceAsStream(component.getImgSrc()));
         componentImg.setImage(image);
         pickComponentCard.setStyle( "-fx-background-color: #"+component.getColor()+";\n" +
-                                    "    -fx-background-radius: 30;");
+                "    -fx-background-radius: 30;");
     }
 
-    private void initMethod(Option option){
-        if(option != Option.SEARCH){
-            searchLabel.setVisible(false);
-        }
-        grid.getChildren().clear();
-        List<Component> components;
-        components = InitComponents.getInstance().getData(option);
+   private void setButton(Option option) {
+       allBtn.getStyleClass().remove("btn-selected");
+       cpuBtn.getStyleClass().remove("btn-selected");
+       gpuBtn.getStyleClass().remove("btn-selected");
+       ramBtn.getStyleClass().remove("btn-selected");
+       keyboardBtn.getStyleClass().remove("btn-selected");
+       mouseBtn.getStyleClass().remove("btn-selected");
+       headphoneBtn.getStyleClass().remove("btn-selected");
+       speakerBtn.getStyleClass().remove("btn-selected");
 
-        if(components.size() > 0){
-            setCard(components.get(0));
-            myListener = new MyListener() {
-                @Override
-                public void onClickListener(Component component) {
-                    setCard(component);
-                }
-            };
-        }
+       switch (option) {
+           case ALL -> allBtn.getStyleClass().add("btn-selected");
+           case MOUSE -> mouseBtn.getStyleClass().add("btn-selected");
+           case KEYBOARD -> keyboardBtn.getStyleClass().add("btn-selected");
+           case HEADPHONE -> headphoneBtn.getStyleClass().add("btn-selected");
+           case CPU -> cpuBtn.getStyleClass().add("btn-selected");
+           case GPU -> gpuBtn.getStyleClass().add("btn-selected");
+           case SPEAKER -> speakerBtn.getStyleClass().add("btn-selected");
+           case RAM -> ramBtn.getStyleClass().add("btn-selected");
+       }
+   }
 
-        int column = 0;
-        int row = 1;
 
-        try {
-            for (Component component : components) {
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(getClass().getResource("component.fxml"));
-                AnchorPane anchorPane = fxmlLoader.load();
-
-                ComponentController componentController = fxmlLoader.getController();
-                componentController.setData(component, myListener);
-
-                if (column == 3) {
-                    column = 0;
-                    row++;
-                }
-
-                grid.add(anchorPane, column++, row);
-
-                GridPane.setMargin(anchorPane, new Insets(10));
-            }
-
-            grid.setMinWidth(Region.USE_COMPUTED_SIZE);
-            grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
-            grid.setMaxWidth(Region.USE_PREF_SIZE);
-
-            grid.setMinHeight(Region.USE_COMPUTED_SIZE);
-            grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
-            grid.setMinHeight(Region.USE_PREF_SIZE);
-
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
