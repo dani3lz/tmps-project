736cc6e1fc625496e3bc6e27d091dedecb372844
dani3lz <zavorot.daniel@isa.utm.md>
2022-05-07 13:52:20 +0200

    update

--- src/main/java/md/dani3lz/tmps_project/MainApplication.java
+++ src/main/java/md/dani3lz/tmps_project/MainApplication.java
@@ -3,7 +3,9 @@ package md.dani3lz.tmps_project;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.controlsfx.control.tableview2.filter.filtereditor.SouthFilter;
 
 import java.io.IOException;
 
@@ -14,6 +16,7 @@ public class MainApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setResizable(false);
         stage.setTitle("PC Store");
+        stage.getIcons().add(new Image(String.valueOf(MainApplication.class.getResource("img/icon.png"))));
         stage.setScene(scene);
         stage.show();
     }
